89680f6d1b068d65ed0f6a40d9bcc1e489b18bbf
Charles Burton <charles.d.burton@gmail.com>
2021-08-13 21:37:46 -0600

    fix: add functions for stream retrieval

--- streams.go
+++ streams.go
@@ -5,3 +5,15 @@ const (
 	reverseDNSStream  = "reversedns.requests"
 	applicationStream = "application.requests"
 )
+
+func GetDiscoveryStream() string {
+	return discoveryStream
+}
+
+func GetReverseDNSStream() string {
+	return reverseDNSStream
+}
+
+func GetApplicationStream() string {
+	return applicationStream
+}
